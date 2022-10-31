d9543adc92dc2f267bd7fbcce32c6e4b3f36bc29
Débora Rocha <debora.rocha@iatec.com>
2022-10-30 22:19:58 -0300

    Integrando os extratos com a API
    Requisição get funcionando

--- bank/src/app/services/transferencia.service.ts
+++ bank/src/app/services/transferencia.service.ts
@@ -1,17 +1,26 @@
+import { Transferencia } from './models/transferencia.model';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransferenciaService {
   private listaTransferencia: any[];
+  private url = 'http://localhost:3000/transferencias';
 
-constructor() {
+constructor(private httpClient: HttpClient) {
   this.listaTransferencia = [];
  }
 
  get transferencias( ) {
   return this.listaTransferencia;
+ }
+
+ todas(): Observable<Transferencia[]>{
+  return this.httpClient.get<Transferencia[]>(this.url); // get vai retonar um observable de todas as transferencias
  }
 
  adicionar(transferencia: any){
